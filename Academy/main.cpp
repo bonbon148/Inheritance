3fb1d4afb0a241f733b96a125db24019ee686666
bonbon148 <townpony608@gmail.com>
2025-12-22 12:28:49 +0300

    Virtual Destructor DONE

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -56,7 +56,7 @@ public:
 		count++;
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human()
+	virtual ~Human()
 	{
 		count--;
 		cout << "HDestructor:\t" << this << endl;
@@ -317,6 +317,10 @@ void main()
 		//group[i]->info();
 		//cout << delimiter << endl;
 		cout << *group[i] << endl;
+	}
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
 	}
 	cout << "Количество участников группы: " << Human::get_count() << endl;
 }
